e88c198917b0bd90764010a6f8b4c229ae98c187
825873719@qq.com <chen_1991@>
2020-12-12 18:21:13 +0800

    递归代码总结提交

---
+++ jqy-item-sysdoc/src/recursion/Recursion.java
@@ -0,0 +1,14 @@
+package recursion;
+
+public class Recursion {
+    public static int f(int n){
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        return f(n-1) + f(n-2);
+    }
+    public static void main(String[] args) {
+        int num = f(7);
+        System.out.println(num);
+    }
+}
+
